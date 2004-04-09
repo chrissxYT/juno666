8d7507aaba98720e52c28da1741df20335167ffe
strepto <strepto>
2004-04-09 07:24:09 +0000

    headers fixed

--- VSTi/juno_keyboard.h
+++ VSTi/juno_keyboard.h
@@ -1,38 +1,9 @@
 #ifndef _JUNO_KEYBOARD_H
 #define _JUNO_KEYBOARD_H
 
-
-//#include <libmoog/moog.h>
-
-#include <libmoog/ADSR.h>
-#include <libmoog/Attenuator.h>
-#include <libmoog/Balance.h>
-#include <libmoog/Chorus.h>
-#include <libmoog/ConnectionInfo.h>
-#include <libmoog/ConnectionList.h>
-#include <libmoog/ConnectionManager.h>
-#include <libmoog/DataBlock.h>
-#include <libmoog/Delay.h>
-#include <libmoog/GoObject.h>
-#include <libmoog/HPF.h>
-#include <libmoog/IIR2.h>
-#include <libmoog/Input.h>
-#include <libmoog/JunoPulse.h>
-#include <libmoog/JunoSaw.h>
-#include <libmoog/Mixer.h>
-#include <libmoog/MoogObject.h>
-#include <libmoog/Oscillator.h>
-#include <libmoog/Output.h>
-#include <libmoog/Rand.h>
-#include <libmoog/ResonantLowPass.h>
-#include <libmoog/Rms.h>
-#include <libmoog/Settings.h>
-#include <libmoog/WaveGen.h>
-#include <libmoog/pitch.h>
+#include <libmoog/moog.h>
 
 #include <libmoog/Scheduler.h>
-
-
 
 class JunoKeyboard: public MoogObject
 {
